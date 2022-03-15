9fb93e7382a4c7e9aa46628d828f631c68fa93c7
Chandanpreet Kaur <69481002+Chandanpreet91@users.noreply.github.com>
2022-03-15 12:48:54 -0700

    Made changes to this file.

--- python_basics.py
+++ python_basics.py
@@ -1,3 +1,35 @@
-import sys
 
-print(sys.version)
+import sys
+# print(sys.version)
+# print("This will be printed")
+# print("This will cause an error")
+# print("This will NOT be printed")
+# print(type(-12))
+# print(type(0))
+# print(sys.float_info)
+# #Convert int to float
+# print(int(20.12)) #20
+# print(float(23)) #23.0
+# print(bool(1)) #True
+# print(bool(0)) #False
+# x = 3 + 2 * 2
+# print(int(False))
+
+#Slicing and stride
+name = "Michael Jackson"
+# print(name[0:4])
+# print(name[::2])
+# print(name[0:5:2])
+# # print(name[1:5:3])
+# print(name +" "+ "is the best")
+# print(3*name)
+# print(name + "\tis the best")
+# print('Michael jackson \\ is the best')
+# print(r'Michael jackson \ is the best')
+
+#String Operations
+# print(name.upper())
+# print(name.replace('Michael',"Jatson"))
+print(name.find("el"))
+print(name.find('Jack'))
+print(name.find('Hey'))
