12eb03bffd94c49b7d984b26301fbb477cd784ce
Chandanpreet Kaur <69481002+Chandanpreet91@users.noreply.github.com>
2022-03-16 18:42:12 -0700

    added other method.

--- lists.py
+++ lists.py
@@ -2,6 +2,12 @@ import random
 
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-total = len(names)
-random_number = random.randint(0,total)
-print(names[random_number])
+# total = len(names)
+# random_number = random.randint(0,total-1)
+# print(names[random_number])
+
+
+#OR
+#choice method is also used to select a random number
+random_name = random.choice(names)
+print(random_name)
